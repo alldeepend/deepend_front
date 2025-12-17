061f8505ef2a4d73fc45527da952536bccaa4bb2
Bismarck Aragon Andrade <bismarck011@gmail.com>
2025-12-17 09:06:19 -0500

    Logos

--- src/components/shared/PublicRoute.tsx
+++ src/components/shared/PublicRoute.tsx
@@ -9,13 +9,13 @@ export default function PublicRoute({ children }: { children: ReactNode }) {
         return <div className="flex items-center justify-center min-h-screen bg-white">
             <div className="relative z-10 max-w-md w-full text-center">
                 <img
-                    src="https://media.100cosasporhacer.com/memories/9701f6ef-c3ee-4454-b2fb-996f0a5deeab-Mil_Momentos_log.webp"
-                    alt="100 Cosas por hacer"
+                    src="https://media.1000momentos.com/memories/69e82978-65ec-4ca5-9397-468483861205-DeepEnd_FondoBlanco.webp"
+                    alt="DeepEnd Logo"
                     className="w-[224px] h-[224px] mx-auto mb-4 rounded-full object-cover animate-pulse-fast"
                 />
-                <h1 className="font-handwriting text-6xl text-stone-800 mb-2 animate-pulse-fast">Mil Momentos</h1>
+                {/* <h1 className="font-handwriting text-6xl text-stone-800 mb-2 animate-pulse-fast">Mil Momentos</h1>
                 <h2 className="font-handwriting text-4xl text-stone-600 mb-6">Historias que dejan huellas</h2>
-                <p className="text-stone-500 font-sans mb-14 px-4"></p>
+                <p className="text-stone-500 font-sans mb-14 px-4"></p> */}
             </div>
         </div>
     }
