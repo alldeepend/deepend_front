88e18492a43fdb927a9ba8749b2111c76a0e2f3c
Bismarck Aragon Andrade <bismarck011@gmail.com>
2026-01-26 12:06:32 -0500

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,44 @@
 import { defineConfig, type PluginOption } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss() as PluginOption],
+  plugins: [
+    react(),
+    tailwindcss() as PluginOption,
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'DeepEnd',
+        short_name: 'DeepEnd',
+        description: 'DeepEnd Platform',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
 })
