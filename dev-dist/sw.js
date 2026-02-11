ca0b42862e46b31e11507c681ec8f5cc6ed0aac4
Bismarck Aragon Andrade <bismarck011@gmail.com>
2026-02-11 13:52:01 -0500

    Actualizacion

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.td3qc5g2cck"
+    "revision": "0.rfm2nai67ro"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
