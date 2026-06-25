963e574c7a3b315f86528622e807bf6624545f01
240-johan <johancruzh@gmail.com>
2026-06-25 11:04:29 -0500

    Merge main into develop

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.fg8jnkde0h4"
+    "revision": "0.eersijr4h98"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
